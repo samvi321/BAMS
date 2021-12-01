1716c8868690d6bdbd3e3cb482f47fec93309094
samvi321 <68268095+samvi321@users.noreply.github.com>
2021-12-01 19:52:16 +0530

    Delete bams directory

--- bams/includes/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost","root","","bams") or die("connection failed".mysqli_connect_error());
-?>
--- bams/includes/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'security.php';
-    session_destroy();
-    unset($_SESSION['email']);
-    unset($_SESSION['accno']);
-    unset($_SESSION['user']);
-    header("location:../index.php");
-?>
